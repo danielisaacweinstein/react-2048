9fee0c43bbbfd69a28a53dfe0f3bfb8e72644acc
Daniel Isaac Weinstein <danielisaacweinstein@gmail.com>
2016-02-01 13:51:45 -0500

    A bunch of boilerplate for WebPack and package.json.

---
+++ js/app.js
@@ -0,0 +1,10 @@
+"use strict"
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { TopComponent } from './components/TopComponent.jsx'
+
+ReactDOM.render(
+  <TopComponent />,
+  document.getElementById('react')
+)
---
+++ js/components/TopComponent.jsx
@@ -0,0 +1,11 @@
+"use strict"
+
+import React from 'react'
+
+export class TopComponent extends React.Component {
+  render() {
+    return (
+      <p>Eureka!</p>
+    );
+  }
+}
