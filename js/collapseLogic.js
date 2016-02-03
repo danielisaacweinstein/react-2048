0e62ebb78269d4f3d262f34b210e6f6919b001b1
Daniel Isaac Weinstein <danielisaacweinstein@gmail.com>
2016-02-03 18:52:11 -0500

    Generalized collapse logic.

---
+++ js/collapseLogic.js
@@ -0,0 +1,113 @@
+import * as Immutable from 'immutable'
+
+function shiftGrid(grid) {
+  var newGrid = grid.map(function(row, rowIndex) {
+    var newRow = row.reduce(function(cellsSoFar, currentCell) {
+      var isEmpty = currentCell === undefined;
+      return isEmpty ? cellsSoFar : cellsSoFar.push(currentCell);
+    }, Immutable.fromJS([]));
+    return newRow;
+  });
+
+  return newGrid;
+}
+
+function collapseGrid(grid) {
+  var newGrid = grid.map(function(row, rowIndex) {
+    var newRow = row.reduce(function(currentRow, currentCell, index, row) {
+      
+      var currentValue = currentRow.get(index);
+      var restOfRow = currentRow.slice(index + 1);
+
+      var fullIndexInRestOfRow = restOfRow.findIndex(function(cell) {
+        return (cell !== undefined) ? true : false;
+      });
+
+      if (fullIndexInRestOfRow !== -1) {
+        var nextIndex = index + fullIndexInRestOfRow + 1;
+        var contentsMatch = (currentRow.get(index) === currentRow.get(nextIndex));
+
+        if (contentsMatch) {
+          currentRow = currentRow.set(index, currentValue * 2);
+          currentRow = currentRow.set(nextIndex, undefined);
+        }
+      }
+      return currentRow;
+    }, row)
+    return newRow;
+  });
+  return newGrid;
+}
+
+function fillGrid(grid) {
+  var newGrid = grid.map(function(row, rowIndex) {
+    while (row.size < 4) { row = row.push(undefined); }
+    return row;
+  });
+
+  return newGrid;
+}
+
+function rotate90(grid) {
+  var lines = [];
+
+  for (var i = 0; i < grid.size; i++) {
+    var line = [];
+    
+    for (var j = grid.size - 1; j >= 0; j--) {
+      line.push(grid.getIn([j, i]));
+    }
+    lines.push(line);
+  }
+ 
+  return Immutable.fromJS(lines);
+}
+
+function getCollapser(direction) {
+  var [down, left, right, up] = [40, 37, 39, 38];
+
+  var firstRotationCount;
+  var secondRotationCount;
+
+  if (direction === left) {
+    firstRotationCount = 0;
+    secondRotationCount = 0;
+  } else if (direction === right) {
+    firstRotationCount = 2;
+    secondRotationCount = 2;
+  } else if (direction === up) {
+    firstRotationCount = 3;
+    secondRotationCount = 1;
+  } else if (direction === down) {
+    firstRotationCount = 1;
+    secondRotationCount = 3;
+  }
+
+  return function(grid) {
+    for (let i = 0; i < firstRotationCount; i++) {
+      grid = rotate90(grid);
+    }
+
+    grid = shiftGrid(grid);
+    grid = collapseGrid(grid);
+    grid = shiftGrid(grid);
+    grid = fillGrid(grid);
+
+    for (let i = 0; i < secondRotationCount; i++) {
+      grid = rotate90(grid);
+    }
+
+    return grid;
+  }
+}
+
+export function getCollapsedGrid(state, incomingData) {
+  var grid = state.get('currentGrid');
+  var direction = incomingData.keyCode;
+  var collapse = getCollapser(direction);
+
+  grid = collapse(grid);
+  state = state.set('currentGrid', grid);
+
+  return state;
+}
--- js/reducer.js
+++ js/reducer.js
@@ -1,9 +1,10 @@
 import * as Immutable from 'immutable'
+import { getCollapsedGrid } from './collapseLogic.js'
 
 function getInitialState(state) {
   var initialState = Immutable.fromJS({
     currentGrid: [
-      [2,2,6,2,],
+      [2,2,2,2,],
       [ , ,2, ,],
       [8, , ,8,],
       [ ,4, , ,]
@@ -13,75 +14,12 @@ function getInitialState(state) {
   return state.merge(initialState);
 }
 
-function shiftGrid(grid) {
-  var newGrid = grid.map(function(row, rowIndex) {
-    var newRow = row.reduce(function(cellsSoFar, currentCell) {
-      var isEmpty = currentCell === undefined;
-      return isEmpty ? cellsSoFar : cellsSoFar.push(currentCell);
-    }, Immutable.fromJS([]));
-    return newRow;
-  });
-
-  return newGrid;
-}
-
-function collapseGrid(grid) {
-  var newGrid = grid.map(function(row, rowIndex) {
-    var newRow = row.reduce(function(currentRow, currentCell, index, row) {
-      
-      var currentValue = currentRow.get(index);
-      var restOfRow = currentRow.slice(index + 1);
-
-      var fullIndexInRestOfRow = restOfRow.findIndex(function(cell) {
-        return (cell !== undefined) ? true : false;
-      });
-
-      if (fullIndexInRestOfRow !== -1) {
-        var nextIndex = index + fullIndexInRestOfRow + 1;
-        var contentsMatch = (currentRow.get(index) === currentRow.get(nextIndex));
-
-        if (contentsMatch) {
-          currentRow = currentRow.set(index, currentValue * 2);
-          currentRow = currentRow.set(nextIndex, undefined);
-        }
-      }
-
-      return currentRow;
-    }, row)
-    return newRow;
-  });
-  return newGrid;
-}
-
-function fillGrid(grid) {
-  var newGrid = grid.map(function(row, rowIndex) {
-    while (row.size < 4) { row = row.push(undefined); }
-    return row;
-  });
-
-  return newGrid;
-}
-
-function getShiftedGrid(state, incomingData) {
-  var newGrid = state.get('currentGrid');
-
-  newGrid = shiftGrid(newGrid);
-  newGrid = collapseGrid(newGrid);
-  newGrid = shiftGrid(newGrid);
-  newGrid = fillGrid(newGrid);
-
-  state = state.set('currentGrid', newGrid);
-
-  return state;
-}
-
-
 function reducer(state = Immutable.Map(), action) {
   switch (action.type) {
     case 'SET_INITIAL_STATE':
       return getInitialState(state);
     case 'SHIFT':
-      return getShiftedGrid(state, action.data);
+      return getCollapsedGrid(state, action.data);
   }
   return state;
 }
