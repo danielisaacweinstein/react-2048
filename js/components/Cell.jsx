bfa4d4c120483660a81f9397c7afdeca9993efb6
Daniel Isaac Weinstein <danielisaacweinstein@gmail.com>
2016-02-10 00:31:34 -0500

    Use module classnames for cell CSS styling.

--- js/components/Cell.jsx
+++ js/components/Cell.jsx
@@ -1,26 +1,29 @@
 "use strict"
 
 import React from 'react'
+import classNames from 'classnames'
 
 export class Cell extends React.Component {
   render() {
-    var colorMap = {
-      2: '#ECF2E6',
-      4: '#DBE9D0',
-      6: '#D2E3C4',
-      8: '#C2D5B3',
-      16: '#C7F2A5',
-      32: '#9BDBD9',
-      64: '#92CBC9',
-      128: '#92A3CB',
-      256: '#D179E5',
-      512: '#D001FF',
-      1024: '#FF01AF',
-      2048: '#FF4D9E',
-    }
+    var num = this.props.contents;
+
+    var cssStyling = classNames({
+      'cell':     true,
+      'cell2':    (num === 2),
+      'cell4':    (num === 4),
+      'cell8':    (num === 8),
+      'cell16':   (num === 16),
+      'cell32':   (num === 32),
+      'cell64':   (num === 64),
+      'cell128':  (num === 128),
+      'cell256':  (num === 256),
+      'cell512':  (num === 512),
+      'cell1024': (num === 1024),
+      'cell2048': (num === 2048)
+    })
 
     return (
-      <td className="cell" style={{backgroundColor: colorMap[this.props.contents]}}>
+      <td className={cssStyling}>
         {this.props.contents}
       </td>
     )
